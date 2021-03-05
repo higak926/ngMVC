db5689eba3a7bb0228aa0e654ae532def3bd7e60
kazuakihiga <higak926@gmail.com>
2021-03-05 15:57:04 +0900

    first commit
    * ngMVC.sln:
    * Program.cs:
    * Startup.cs:
    * ngMVC.csproj:
    * appsettings.json:
    * site.js:
    * README.md:
    * .gitignore:
    * favicon.ico:
    * test.ts:
    * tslint.json:
    * site.css:
    * main.ts:
    * package.json:
    * angular.json:
    * .editorconfig:
    * karma.conf.js:
    * tsconfig.json:
    * index.html:
    * styles.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * favicon.ico:
    * .browserslistrc:
    * _ViewImports.cshtml:
    * Privacy.cshtml:
    * Error.cshtml:
    * polyfills.ts:
    * app.po.ts:
    * tsconfig.json:
    * package-lock.json:
    * tsconfig.app.json:
    * tsconfig.spec.json:
    * _Layout.cshtml:
    * .gitkeep:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * app.module.ts:
    * LICENSE.txt:
    * launchSettings.json:
    * protractor.conf.js:
    * app.e2e-spec.ts:
    * app.component.ts:
    * jquery.js:
    * app.component.css:
    * app.component.html:
    * jquery.min.js:
    * jquery.min.map:
    * app-routing.module.ts:
    * app.component.spec.ts:
    * environment.ts:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * environment.prod.ts:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap.bundle.js:
    * bootstrap-grid.css:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap-grid.min.css:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ ngMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ngMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
